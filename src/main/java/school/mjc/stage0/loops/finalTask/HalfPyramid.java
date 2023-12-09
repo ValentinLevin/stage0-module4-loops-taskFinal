a21b3a7b3d4d540b87e3591fd4866e89849e53db
Valentin Levin <levin_valentin@mail.ru>
2023-12-09 10:33:59 +0600

    task completed

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int row = 1; row <= cathetusLength; row++) {
+            for (int col = cathetusLength; col >= 1; col--) {
+                if (col <= row) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
 
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int row = 1; row <= cathetusLength; row++) {
+            for (int col = cathetusLength; col >= 1; col--) {
+                if (col <= row) {
+                    System.out.print(col);
+                } else {
+                    System.out.print(' ');
+                }
+            }
 
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col);
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
